9d0de12195476b9e37fc5dfcc26d16363d7b9200
waxz <wz67766@163.com>
2018-12-21 10:09:38 +0800

    add py

---
+++ HMM.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+#%%
+import numpy as np
+
+#%%
+# 随机生成观测序列和状态序列    
+def simulate(T, A, B, pi):
+    print("simulat model:A:\n", A, "\nB\n", B, "\npi\n", pi)
+
+    def draw_from(probs):
+        """
+        1.np.random.multinomial:
+        按照多项式分布，生成数据
+        >>> np.random.multinomial(20, [1/6.]*6, size=2)
+                array([[3, 4, 3, 3, 4, 3],
+                       [2, 4, 3, 4, 0, 7]])
+         For the first run, we threw 3 times 1, 4 times 2, etc.  
+         For the second, we threw 2 times 1, 4 times 2, etc.
+        2.np.where:
+        >>> x = np.arange(9.).reshape(3, 3)
+        >>> np.where( x > 5 )
+        (array([2, 2, 2]), array([0, 1, 2]))
+        """
+        return np.where(np.random.multinomial(1,probs) == 1)[0][0]
+
+    observations = np.zeros(T, dtype=int)
+    states = np.zeros(T, dtype=int)
+    states[0] = draw_from(pi)
+    observations[0] = draw_from(B[states[0],:])
+    for t in range(1, T):
+        states[t] = draw_from(A[states[t-1],:])
+        observations[t] = draw_from(B[states[t],:])
+    return observations, states
+
+
+# ## offline 
+#%%
+def forward(obs_seq, A, B, pi):
+    """前向算法"""
+    N = A.shape[0]
+    T = len(obs_seq)
+    
+    # F保存前向概率矩阵
+    F = np.zeros((N,T))
+    F[:,0] = pi * B[:, obs_seq[0]]
+
+    for t in range(1, T):
+        for n in range(N):
+            F[n,t] = np.dot(F[:,t-1], (A[:,n])) * B[n, obs_seq[t]]
+
+    return F
+
+def backward(obs_seq, A, B):
+    """后向算法"""
+    N = A.shape[0]
+    T = len(obs_seq)
+    # X保存后向概率矩阵
+    X = np.zeros((N,T))
+    X[:,-1:] = 1
+#     print("x: ", X)
+
+    for t in reversed(range(T-1)):
+        for n in range(N):
+#             dd = X[:,t+1] * A[n,:] * B[:, obs_seq[t+1]]
+#             print(dd)
+            X[n,t] = np.sum(X[:,t+1] * A[n,:] * B[:, obs_seq[t+1]])
+
+    return X
+
+
+# In[11]:
+
+
+def baum_welch_train(observations, A, B, pi, criterion=0.05):
+    """无监督学习算法——Baum-Weich算法"""
+    n_states = A.shape[0]
+    n_samples = len(observations)
+
+    done = False
+    while not done:
+        # alpha_t(i) = P(O_1 O_2 ... O_t, q_t = S_i | hmm)
+        # Initialize alpha
+        alpha = forward(observations,A,B,pi)
+
+        # beta_t(i) = P(O_t+1 O_t+2 ... O_T | q_t = S_i , hmm)
+        # Initialize beta
+        beta = backward(observations, A, B)
+        # ξ_t(i,j)=P(i_t=q_i,i_{i+1}=q_j|O,λ)
+        xi = np.zeros((n_states,n_states,n_samples-1))
+        for t in range(n_samples-1):
+            denom = np.dot(np.dot(alpha[:,t].T, A) * B[:,observations[t+1]].T, beta[:,t+1])
+            for i in range(n_states):
+                numer = alpha[i,t] * A[i,:] * B[:,observations[t+1]].T * beta[:,t+1].T
+                xi[i,:,t] = numer / denom
+
+        # γ_t(i)：gamma_t(i) = P(q_t = S_i | O, hmm)
+        gamma = np.sum(xi,axis=1)
+        # Need final gamma element for new B
+        # xi的第三维长度n_samples-1，少一个，所以gamma要计算最后一个
+        prod =  (alpha[:,n_samples-1] * beta[:,n_samples-1]).reshape((-1,1))
+        gamma = np.hstack((gamma,  prod / np.sum(prod))) #append one more to gamma!!!
+        
+        # 更新模型参数
+        newpi = gamma[:,0]
+        newA = np.sum(xi,2) / np.sum(gamma[:,:-1],axis=1).reshape((-1,1))
+        newB = np.copy(B)
+        num_levels = B.shape[1]
+        sumgamma = np.sum(gamma,axis=1)
+        for lev in range(num_levels):
+            mask = observations == lev
+            newB[:,lev] = np.sum(gamma[:,mask],axis=1) / sumgamma
+        
+        # 检查是否满足阈值
+        if np.max(abs(pi - newpi)) < criterion and \
+           np.max(abs(A - newA)) < criterion and \
+           np.max(abs(B - newB)) < criterion:
+            done = 1
+        A[:], B[:], pi[:] = newA, newB, newpi
+    return newA, newB, newpi
+
+#%%
+A_ = np.array([[0.3, 0.7],[0.4, 0.6]])
+B_ = np.array([[0.1, 0.3, 0.6],[0.4, 0.5, 0.1]])
+pi_ = np.array([0.5, 0.5])
+# print("pi: ", pi)
+# pic = pi.copy()
+#%%
+observations_data, states_data = simulate(500,A_, B_, pi_)
+print("observations_data:\n",observations_data)
+print("states_data: \n",states_data)
+#%%
+# pa = A_.copy() #np.zeros_like(A_)
+# pb = B_.copy() #np.zeros_like(B_)
+# pc = np.ones_like(pi_)
+
+# pa = np.zeros_like(A_)
+# pb = np.zeros_like(B_)
+# pc = np.ones_like(pi_)
+
+pa = np.array([[0.2, 0.8],[0.5, 0.5]])
+pb = np.array([[0.3, 0.3, 0.4],[0.4, 0.3, 0.3]])
+pc = pi_.copy() #np.ones_like(pi_)
+newA, newB, newpi = baum_welch_train(observations_data, pa, pb, pc,0.05)
+print("newA: ", newA)
+print("newB: ", newB)
+print("newpi: ", newpi)
+
+#%%
+for i in [0,1,2]:
+    print(i, " observations_data-cnt", (observations_data == i).sum())
+
+for i in [0,1]:
+    print(i, " states_data-cnt", (states_data == i).sum())
+
+
+
+
+#%%
---
+++ HMM2.py
@@ -0,0 +1,232 @@
+#%%
+from HMM import baum_welch_train
+import numpy as np
+# ## online
+# Online Learning with Hidden Markov Models[ Gianluigi Mongillo ,Sophie Deneve]
+
+# Start with: 
+# - initial guess for the model parameters, θ(0) = [A,B], 
+# - initial state probabilities, ˆql (0) ≡ P(x0 = l), 
+# - sufficient statistics, ˆφh ijk(0)
+
+# specific data struct
+# 
+
+
+#%%
+def delta(i, j):
+    if i == j:
+        return 1
+    else :
+        return 0
+def g_delta(i, j, l, h):
+    return delta(i, l )*delta(j, h)
+
+
+class Q_data:
+    def __init__(self,Q_):
+        self.Q = Q_
+    
+    def updateQ(self, new_obs_k, Gama_):
+        Q0 = self.Q
+        print("update Q with K: ", new_obs_k, "\nGama: \n", Gama_, "\nQ0: \n", Q0)
+        self.Q[0] = (Gama_[0][0][new_obs_k] * Q0[0]) + (Gama_[1][0][new_obs_k] * Q0[1])  
+
+        self.Q[1] = (Gama_[0][1][new_obs_k] * Q0[0]) + (Gama_[1][1][new_obs_k] * Q0[1])  
+        
+        print("updated Q = \n", self.Q)
+
+    def getQ(self):
+        return self.Q
+
+
+# In[32]:
+
+
+class Gama_data:
+    def __init__(self, Gama_):
+        self.Gama = Gama_
+        self.prob_sum = None
+        self.A = None
+        self.B = None
+    
+    def updateModel(self, A_, B_):
+        self.A = A_
+        self.B = B_
+        
+    def updateGama(self, new_obs_k, Q_old):
+        A_ = self.A
+        B_ = self.B
+        Q_ = Q_old
+        print("update Gama with k = ", new_obs_k, "\n Q = ", Q_, "\n A : \n", A_, "\n B: \n", B_)
+        prob_sum = (A_[0][0]*B_[0][new_obs_k]*Q_[0] + A_[0][1]*B_[1][new_obs_k]*Q_[0]\
+                  + A_[1][0]*B_[0][new_obs_k]*Q_[1] + A_[1][1]*B_[1][new_obs_k]*Q_[1]  )
+        self.prob_sum = prob_sum
+        self.Gama[0][0][new_obs_k] = (A_[0][0]*B_[0][new_obs_k]) / prob_sum
+
+        self.Gama[0][1][new_obs_k] = (A_[0][1]*B_[1][new_obs_k]) / prob_sum
+
+        self.Gama[1][0][new_obs_k] = (A_[1][0]*B_[0][new_obs_k]) / prob_sum
+
+        self.Gama[1][1][new_obs_k] = (A_[1][1]*B_[1][new_obs_k]) / prob_sum
+        print("updated Gama = \n", self.Gama)
+        
+    
+    
+    def get(self, i_, j_, k_):
+        return self.Gama[i_][j_][k_]
+        
+    def getGama(self):
+        return self.Gama
+    
+# how to define a datastruct
+# member data
+# method
+
+# for Gama
+# contatin latest Gama
+# updata Gama from Q and yT, A, B
+
+
+# In[42]:
+
+
+class Fi_data:
+    def __init__(self, Fi_, time_factor_):
+        self.Fi = Fi_
+        self.time_factor = time_factor_
+        
+    def updateMode(self,new_obs_k ,Gama_, Q_):
+        for i in range(self.Fi.shape[0]):
+            for j in range(self.Fi.shape[1]):
+                for h in range(self.Fi.shape[2]):
+                    for k in range(self.Fi.shape[3]):
+                        self.Fi[i][j][h][k]  = (Gama_[0][h][k] * (self.Fi[i][j][0][k] + self.time_factor*(delta(new_obs_k, k)*g_delta(i, j ,0, h)*Q_[0] - self.Fi[i][j][0][k] ) ) ) +                                                (Gama_[1][h][k] * (self.Fi[i][j][1][k] + self.time_factor*(delta(new_obs_k, k)*g_delta(i, j ,1, h)*Q_[1] - self.Fi[i][j][1][k] ) ) )
+                
+    def getFi(self):
+        return self.Fi;
+
+
+# In[43]:
+
+# In[62]:
+
+#%%
+class HmmTrainer:
+    def __init__(self, state_dim_, obs_dim_):
+        transition_probability = {
+            'Healthy': {'Healthy': 0.7, 'Fever': 0.3},
+            'Fever': {'Healthy': 0.4, 'Fever': 0.6},
+        }
+        # 观测概率矩阵B
+        emission_probability = {
+            'Healthy': {'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1},
+            'Fever': {'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6},
+        }
+        print("transition_probability:\n", transition_probability)
+        print("emission_probability:\n", emission_probability)
+        self.A = np.array([[0.7, 0.3],
+                           [0.4, 0.6]])
+        self.B = np.array([[0.5, 0.4, 0.1 ],
+                           [0.1, 0.3, 0.6]])
+        
+        self.A = np.array([[0.5, 0.5],
+                           [0.5, 0.5]])
+        self.B = np.array([[0.3, 0.3, 0.3 ],
+                           [0.3, 0.3, 0.3]])
+
+        self.pi = np.ones([state_dim_])/ float(state_dim_)
+        
+        print("A:\n", self.A, "\nB:\n", self.B, "\npi:\n", self.pi)
+        
+        Gama_ = 0.0*np.ones([2,2,3])
+
+        print("Gama:\n", Gama_)
+        
+        self.gama = Gama_data(Gama_)
+        
+        Q_ = self.pi
+        
+        self.q = Q_data(Q_)
+        
+        Fi_ = np.zeros([state_dim_,state_dim_,state_dim_,obs_dim_])/float(obs_dim_)
+        print("信息概率:\n ", Fi_)
+        
+        time_factor_ = 0.001
+
+        print("time_factor: ", time_factor_)
+        
+        self.fi = Fi_data(Fi_, time_factor_)
+        
+        print("Fuck===========")
+#         self.Q 
+#         self.Gama
+
+    def online_train(self, data_):
+            for ob in data_:
+    #             updata gama
+                self.gama.updateModel(self.A,self.B)
+        
+    #             print("debug gama:\n", self.gama.getGama(),self.q.getQ())
+                self.gama.updateGama(new_obs_k=ob,Q_old=self.q.getQ())
+
+
+
+    #             updata q
+                self.q.updateQ(ob, self.gama.getGama())
+
+    #             update fi
+                self.fi.updateMode(ob, self.gama.getGama(), self.q.getQ())
+            
+    #             continue
+    #             if not np.any(Gama == 0.0):
+    # #             update A, B
+    #                 print("update A B")
+                
+    #                 fi = self.fi.getFi()
+    #             # fi(i,j,h,k)
+    #                 for i in range(self.A.shape[0]):
+    #                     for j in range(self.A.shape[1]):
+    #                         self.A[i][j] = fi[i,j,:,:].sum() / fi[i,:,:,:].sum()
+                    
+    #                 for j in range(self.B.shape[0]):
+    #                     for k in range(self.B.shape[1]):
+    #                         self.B[j][k] = fi[:,j,:,k].sum() / fi[:,j,:,:].sum()
+
+
+
+        
+
+
+    def learn(self, data_, online = False):
+        
+        print("get data:\n", data_, "train_mode online = ", online)
+
+        if not online:
+            
+            self.A, self.B, self.pi = baum_welch_train(data_, self.A, self.B, self.pi, criterion=0.05)
+        
+        else :
+            self.online_train(data_)
+
+
+    
+        
+#         only when all possible observation is detected
+            
+
+
+
+
+#%%
+h = HmmTrainer(2,3)
+
+#%%
+y = np.array([0,1,0,1,1,1])
+
+h.learn(y, True)
+
+#%%
+
+
+#%%
